1dbaace41ff81c310653c391dc961f5a82c995ff
Miguel Alvarez <malvarez@uni-bonn.de>
2016-04-08 10:23:21 +0200

    New version of 'vegtable' objects

--- R/1_vegtable-class.R
+++ R/1_vegtable-class.R
@@ -7,27 +7,24 @@ setClass("vegtable", slots=c(
 				description="character",
 				samples="data.frame",
 				head="data.frame",
-				species="taxlist",
+				species="ANY",
 				popups="list",
-				coverconvert="list",
+				coverconvert="ANY",
+                syntax="ANY",
 				log="list"),
 		prototype=list(description=character(), samples=data.frame(),
-				head=data.frame(), species=new("taxlist"), popups=list(),
-				coverconvert=list(), log=list()),
+				head=data.frame(), species=NULL, popups=list(),
+				coverconvert=NULL, syntax=NULL, log=list()),
 		validity=function(object) {
 			if(!is.character(object@description))
-				return("slot description should be a character vector")
+				return("slot 'description' should be a character vector")
 			if(!is.data.frame(object@samples))
-				return("slot samples should be a data frame")
+				return("slot 'samples' should be a data frame")
 			if(!is.data.frame(object@head))
-				return("slot head should be a data frame")
-			if(class(object@species) != "taxlist")
-				return("slot species should be a taxlist")
+				return("slot 'head' should be a data frame")
 			if(!is.list(object@popups))
-				return("slot popups should be a list")
-			if(!is.list(object@coverconvert))
-				return("slot coverconvert should be list")
+				return("slot 'popups' should be a list")
 			if(!is.list(object@log))
-				return("slot log should be a list")
+				return("slot 'log' should be a list")
         }
 )
