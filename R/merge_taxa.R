7c7cbbce7f50e526601130ec94405423d0da68af
Miguel Alvarez <malvarez@uni-bonn.de>
2016-05-06 12:12:24 +0200

    Change in function name
    merge_species to merge_taxa

--- R/merge_taxa.R
+++ R/merge_taxa.R
@@ -1,13 +1,13 @@
-# TODO:   Method of merge_species for vegtable objects
+# TODO:   Method of merge_taxa for vegtable objects
 # 
 # Author: Miguel Alvarez
 ################################################################################
 
-setMethod("merge_species", signature(taxlist="vegtable"),
+setMethod("merge_taxa", signature(taxlist="vegtable"),
         function(taxlist, ...) {
             if(class(taxlist@species) != "taxlist")
                 stop("'taxlist' should be an object of class vegtable")
-            taxlist@species <- merge_species(taxlist@species)
+            taxlist@species <- merge_taxa(taxlist@species)
             return(taxlist)
         }
 )
