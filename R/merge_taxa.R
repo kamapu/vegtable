081a3a3fc3730830ad2c4cd41368914afd9e881d
Miguel Alvarez <malvarez@uni-bonn.de>
2017-01-17 12:32:39 +0100

    Small bug in merge_taxa

--- R/merge_taxa.R
+++ R/merge_taxa.R
@@ -3,11 +3,11 @@
 # Author: Miguel Alvarez
 ################################################################################
 
-setMethod("merge_taxa", signature(taxlist="vegtable"),
-        function(taxlist, ...) {
-            if(class(taxlist@species) != "taxlist")
-                stop("'taxlist' should be an object of class vegtable")
-            taxlist@species <- merge_taxa(taxlist@species)
-            return(taxlist)
+setMethod("merge_taxa", signature(object="vegtable"),
+        function(object, ...) {
+            if(class(object@species) != "taxlist")
+                stop("'object' should be an object of class vegtable")
+            object@species <- merge_taxa(object@species)
+            return(object)
         }
 )
