29803ec6ca816d1dc06bac252d7ab1b8ee701ec0
Miguel Alvarez <kamapu78@gmail.com>
2019-03-03 23:30:26 +0100

    Adjustments to function 'write_juice'

--- R/write_juice.R
+++ R/write_juice.R
@@ -10,16 +10,17 @@ setGeneric("write_juice", function(data, file, formula, ...)
 # Method for data frames
 setMethod("write_juice", signature(data="vegtable", file="character",
                 formula="formula"),
-        function(data, file, formula, header=c("COUNTRY","REFERENCE"),
-                coords=c("LONGITUDE","LATITUDE"), FUN, ...) {
+        function(data, file, db_name="Plot Observations", formula, FUN,
+				header, coords, ...) {
             # some attributes
-            db_name <- data@description["db_name"]
             nr.plots <- nrow(data@header)
             # header
             header.in <- header
             if(attr(terms(formula), "term.labels")[1] != "ReleveID")
                 stop("'ReleveID' is mandatory as first term of formula")
-            if(length(coords) == 2) {
+            if(!missing(coords)) {
+				if(length(coords) != 2)
+					stop("Argument 'coords' have to be of length 2.")
                 header <- c(header, coords)
                 header.in <- c(header.in, c("deg_lon","deg_lat"))
             }
