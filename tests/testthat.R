b899e5a07623a6b3e9f30ffec7747f2643a6e19b
Miguel Alvarez <kamapu78@gmail.com>
2021-01-06 00:12:42 +0100

    Preparing for ropensci

---
+++ tests/testthat.R
@@ -0,0 +1,7 @@
+## browseURL(paste0("https://walczak.org/2017/06/",
+##                 "how-to-add-code-coverage-codecov-to-your-r-package/"))
+
+library(testthat)
+library(vegtable)
+
+test_check("vegtable")
